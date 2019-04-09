364fa917f606c358f9a369bb6bc9bddba3e28885
Alvian Joseph <alvianjoseph@gmail.com>
2019-04-08 20:10:53 -0700

    Day 1 branch

--- app.js
+++ app.js
@@ -1,49 +1,57 @@
 'use strict';
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-var months = ['Jan', 'Feb', 'March'];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+var firstAndPike = {
+  id: 'first-and-pike',
+  maxCookiesSold: 100,
+  maxCustomerPerHour: 65,
 
-// Vinicio - all of the variables and functions related to 2017 are going to live here
-var twoThousandSeventeen = {
-  kms: [2, 4, 5],
-  id: 'ul2017',
-  render: function() {
-    // Vinicio - this function will be in charge of displaying elements into the screen by changing the dome.
+  customersEachHour: [],
+  cookiesEachHour: [],
+  
+  cookiesSoldThisHour: 0,
+  customersThisHour: 0,
+  //avgCookiesPerHour: 0,
 
-    for(var i =0; i < this.kms.length; i++) {
-      // Vinicio - 1 - getting a reference to a part I want to change
-      var referenceUl = document.getElementById(this.id);
+  totalCookiesSold: 0,
 
-      //Vinicio - 2 - Creating the new element
-      var newElement = document.createElement('li'); 
-      newElement.textContent = `In ${months[i]}, I ran ${this.kms[i]} Kilometers.`; 
-
-      //Vinicio - Add the element to your reference
-      referenceUl.append(newElement);
-    }
-  },
+  cookiesSoldEachHour: function (){
+    for(var i = 0; i < hours.length; i++){
+        var customersThisHour = getRandomInt(this.maxCustomerPerHour);
+        var cookiesSoldThisHour = getRandomInt(this.maxCookiesSold);
+        this.totalCookiesSold += cookiesSoldThisHour; 
+        this.customersEachHour.push(customersThisHour)
+        this.cookiesEachHour.push(cookiesSoldThisHour)
+       // var avgCookiesPerHour = Math.round(this.totalCookiesSold/hours.length);
+        console.log(`At ${hours[i]} you sold ${cookiesSoldThisHour} cookies to ${customersThisHour} customers.`);
+        
+      } console.log(`You sold ${this.totalCookiesSold} cookies in total`);
+        
+  }
 };
 
-var twoThousandEighteen = {
-  kms: [12, 14, 15],
-  id: 'ul2018',
-  render: function() {
-    // Vinicio - this function will be in charge of displaying elements into the screen by changing the dome.
+firstAndPike.render = function(){
+  var referenceUl = document.getElementById(this.id);
+  for(var i =0; i < hours.length; i++) {
+  
 
-    for(var i =0; i < this.kms.length; i++) {
-      // Vinicio - 1 - getting a reference to a part I want to change
-      var referenceUl = document.getElementById(this.id);
+    //Vinicio - 2 - Creating the new element
+    var newElement = document.createElement('li'); 
+    newElement.textContent = `At ${hours[i]} you sold ${this.cookiesEachHour[i]} cookies to ${this.customersEachHour[i]} customers.`;
 
-      //Vinicio - 2 - Creating the new element
-      var newElement = document.createElement('li'); 
-      newElement.textContent = `In ${months[i]}, I ran ${this.kms[i]} Kilometers.`; 
-
-      //Vinicio - Add the element to your reference
-      referenceUl.append(newElement);
-    }
-  },
-};
-
-twoThousandSeventeen.render();
-twoThousandEighteen.render();
+    //Vinicio - Add the element to your reference
+    referenceUl.append(newElement);
+  }
+  newElement = document.createElement('li');
+  newElement.textContent = `You sold ${this.totalCookiesSold} cookies in total`;
+  referenceUl.append(newElement);
+}
+  
+ 
+ firstAndPike.cookiesSoldEachHour();
+ firstAndPike.render();
